524936a9ff44b43f257252773c3942d4d03fe485
krayzie <edwinh300@gmail.com>
2022-04-05 16:02:05 -0700

    testing server

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const http = require("http");
+const http = require("http"); // assigns the http protocole to http var
+
 http.createServer((req,res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+    var path = req.url.toLowerCase(); // the url itself
+
+    switch(path) { // this switch case will redirect depending on url
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Home page info');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('My name is Edwin.');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not found');
+            break;
+    }
+
+}).listen(process.env.PORT || 3000); // the .listen chains the functions together
